b5bfcaf6c4d7e0cc30be20ffc50ef669305cfc85
Naguib <ahmedmaelesawy@gmail.com>
2023-10-11 16:40:21 +0300

    Revert changes

--- Sources/NetworkingLayer/BaseMainResponse.swift
+++ Sources/NetworkingLayer/BaseMainResponse.swift
@@ -8,73 +8,39 @@
 import Foundation
 import SmilesBaseMainRequestManager
 
-//open class BaseMainResponse : Codable {
-//
-//    public var additionalInfo : [BaseMainResponseAdditionalInfo]?
-//    public var responseCode : String?
-//    public var responseMsg : String?
-//    public var errorCode : String?
-//    public var errorMsg : String?
-//    public var errorTitle: String?
-//    public var extTransactionId: String?
-//
-//
-//    private enum CodingKeys: String, CodingKey {
-//        case additionalInfo = "additionalInfo"
-//        case responseCode = "responseCode"
-//        case responseMsg = "responseMsg"
-//        case errorCode = "errorCode"
-//        case errorMsg = "errorMsg"
-//        case errorTitle = "errorTitle"
-//        case extTransactionId
-//    }
-//
-//    public required init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        additionalInfo = try values.decodeIfPresent([BaseMainResponseAdditionalInfo].self, forKey: .additionalInfo)
-//        responseCode = try values.decodeIfPresent(String.self, forKey: .responseCode)
-//        responseMsg = try values.decodeIfPresent(String.self, forKey: .responseMsg)
-//        errorCode = try values.decodeIfPresent(String.self, forKey: .errorCode)
-//        errorMsg = try values.decodeIfPresent(String.self, forKey: .errorMsg)
-//        errorTitle = try values.decodeIfPresent(String.self, forKey: .errorTitle)
-//        extTransactionId = try values.decodeIfPresent(String.self, forKey: .extTransactionId)
-//    }
-//
-//    public init() {}
-//
-//}
-
-public protocol BaseMainResponse: Codable {
-    var additionalInfo : [BaseMainResponseAdditionalInfo]? { get set }
-    var responseCode : String? { get set }
-    var responseMsg : String? { get set }
-    var errorCode : String? { get set }
-    var errorMsg : String? { get set }
-    var errorTitle: String? { get set }
-    var extTransactionId: String? { get set }
+open class BaseMainResponse : Codable {
+    
+    public var additionalInfo : [BaseMainResponseAdditionalInfo]?
+    public var responseCode : String?
+    public var responseMsg : String?
+    public var errorCode : String?
+    public var errorMsg : String?
+    public var errorTitle: String?
+    public var extTransactionId: String?
+    
+    
+    private enum CodingKeys: String, CodingKey {
+        case additionalInfo = "additionalInfo"
+        case responseCode = "responseCode"
+        case responseMsg = "responseMsg"
+        case errorCode = "errorCode"
+        case errorMsg = "errorMsg"
+        case errorTitle = "errorTitle"
+        case extTransactionId
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        additionalInfo = try values.decodeIfPresent([BaseMainResponseAdditionalInfo].self, forKey: .additionalInfo)
+        responseCode = try values.decodeIfPresent(String.self, forKey: .responseCode)
+        responseMsg = try values.decodeIfPresent(String.self, forKey: .responseMsg)
+        errorCode = try values.decodeIfPresent(String.self, forKey: .errorCode)
+        errorMsg = try values.decodeIfPresent(String.self, forKey: .errorMsg)
+        errorTitle = try values.decodeIfPresent(String.self, forKey: .errorTitle)
+        extTransactionId = try values.decodeIfPresent(String.self, forKey: .extTransactionId)
+    }
+    
+    public init() {}
+    
 }
 
-
-public extension BaseMainResponse {
-    var additionalInfo : [BaseMainResponseAdditionalInfo]? {
-        []
-    }
-    var responseCode : String? {
-        nil
-    }
-    var responseMsg : String? {
-        nil
-    }
-    var errorCode : String? {
-        nil
-    }
-    var errorMsg : String?  {
-        nil
-    }
-    var errorTitle: String? {
-        nil
-    }
-    var extTransactionId: String? {
-        nil
-    }
-}
