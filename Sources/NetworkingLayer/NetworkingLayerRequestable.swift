34f43e85661e939c8fa653b21e4f4150e1152bbf
Hanan Ahmed <ahanan@etisalat.ae>
2023-10-19 16:30:59 +0500

    -- Added localized description for error

--- Sources/NetworkingLayer/NetworkingLayerRequestable.swift
+++ Sources/NetworkingLayer/NetworkingLayerRequestable.swift
@@ -62,7 +62,7 @@ public class NetworkingLayerRequestable: NSObject, Requestable {
             .decode(type: T.self, decoder: JSONDecoder())
             .mapError { error in
                 // return error if json decoding fails
-                NetworkError.noResponse(String(describing: error))
+                NetworkError.noResponse(String(describing: error.localizedDescription))
             }
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
